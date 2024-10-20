775f85d37c405d0c53d0508614e1720f70a52aee
yamasaki <shohei615615@gmail.com>
2024-10-20 14:21:38 +0900

    api叩く処理追加

---
+++ js/app.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  const requestButton = document.getElementById("js-weather-request");
+  requestButton.addEventListener("click", function () {
+    //api叩く
+    requestData();
+  });
+};
+
+async function requestData() {
+  //フェッチするリソースの定義
+  const url = "https://weather.tsukumijima.net/api/forecast/city/270000";
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`レスポンスステータス: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
