1bd00015b4b39f04c6fbc1d6bb610ae410ae3c2e
yamasaki <shohei615615@gmail.com>
2024-11-27 23:05:35 +0700

    urlに各都市のidを格納

--- js/app.js
+++ js/app.js
@@ -2,17 +2,22 @@ window.onload = function () {
   const requestButton = document.getElementById("js-weather-request");
   requestButton.addEventListener("click", function () {
     //入力された都市名を取得
-    const cityName = document.getElementById("js-input").value;
+    //const cityName = document.getElementById("js-input").value;
+
+    //ドロップダウンメニューの都市のidを取得
+    const dropDown = document.getElementById("js-choose-city");
+    const cityId = dropDown.value;
 
     //api叩く
-    requestData();
+    requestData(cityId);
   });
 };
 
-async function requestData() {
+async function requestData(cityId) {
   //フェッチするリソースの定義
-  const url = "https://weather.tsukumijima.net/api/forecast/city/270000";
+  const url = `https://weather.tsukumijima.net/api/forecast/city/${cityId}`;
 
+  console.log("url: " + url);
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -23,19 +28,15 @@ async function requestData() {
 
     const data = json["forecasts"][0]["dateLabel"];
 
-    console.log(data);
+    //console.log(data);
   } catch (error) {
     console.error(error.message);
   }
 }
 
-function searchPlace(cityName) {
-  fetch();
-}
-
 /*
 天気予報アプリ作成手順
-1. プルダウンで都市を選択できるようにする
-2. 選択した都市をキーとして該当するidを取得
+1. プルダウンで都市を選択できるようにする　完了
+2. 選択した都市をキーとして該当するidを取得　完了
 3. 取得したidをurlの末尾に入れる
 */
