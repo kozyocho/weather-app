c2eb3872c9fd9a60571fdb80e70a82c570ef65f8
yamasaki <shohei615615@gmail.com>
2024-12-02 18:55:07 +0700

    JSONからデータを探す処理追加

--- js/app.js
+++ js/app.js
@@ -2,61 +2,96 @@ window.onload = function () {
   const requestButton = document.getElementById("js-weather-request");
   requestButton.addEventListener("click", async function () {
     //ドロップダウンメニューの都市のidを取得
-    const dropDown = document.getElementById("js-choose-city");
-    const cityId = dropDown.value;
+    //const dropDown = document.getElementById("js-choose-city");
+    //const cityId = dropDown.value;
+
+    //入力された文字列を取得
+    const inputField = document.getElementById("js-input-city");
+    const cityName = inputField.value.trim();
+
+    if (!cityName) {
+      alert("都市を入力してください。");
+      return;
+    }
+
+    await searchJSON(cityName);
 
     //api叩く
-    const weatherData = await requestData(cityId);
+    //const weatherData = await requestData(cityId);
 
     //取得したデータをHTMLにする。requestDataが完了するまで実行されない。
-    setDataToHtml(weatherData);
+    //setDataToHtml(weatherData);
   });
 };
 
-async function requestData(cityId) {
-  //フェッチするリソースの定義
-  //const url = `https://weather.tsukumijima.net/api/forecast/city/${cityId}`;
+async function searchJSON(cityName) {
+  //JSONファイルを読み込む
+  fetch("../cityName.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("JSONファイルの読み込みに失敗しました");
+      }
+      //console.log("json取得失敗");
+      return response.json();
+    })
+    .then((data) => {
+      // JSONデータから部分一致するデータを探す
+      const result = data.find((item) => item.name.includes(cityName));
+      console.log("json取得成功");
 
-  const API_KEY = process.env.OPENWEATHER_API_KEY;
-  //openweatherapi
-  const url = `https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid=${API_KEY}`;
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`レスポンスステータス: ${response.status}`);
-    }
-
-    const json = await response.json();
-
-    //都市名取得
-    const cityName = json.location.city;
-
-    //日にちを取得
-    const date = json.forecasts[0].date;
-
-    //天気画像取得
-    const weatherImage = json.forecasts[0].image.url;
-
-    //最高気温取得
-    const maxTemp = json.forecasts[0].temperature.max.celsius;
-
-    //最低気温取得
-    const minTemp = json.forecasts[0].temperature.min.celsius;
-
-    console.log(json);
-    return {
-      cityName: cityName,
-      date: date,
-      weatherImage: weatherImage,
-      maxTemp: maxTemp,
-      minTemp: minTemp,
-    };
-  } catch (error) {
-    console.error(error.message);
-    return null;
-  }
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error("エラー:", error);
+    });
 }
+
+// async function requestData(cityId) {
+//   //フェッチするリソースの定義
+//   //const url = `https://weather.tsukumijima.net/api/forecast/city/${cityId}`;
+
+//   const API_KEY = process.env.OPENWEATHER_API_KEY;
+//   //openweatherapi
+//   //const url = `https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid=${API_KEY}`;
+
+//   const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&units=metric&appid=${API_KEY}`;
+
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`レスポンスステータス: ${response.status}`);
+//     }
+
+//     const json = await response.json();
+
+//     //都市名取得
+//     const cityName = json.location.city;
+
+//     //日にちを取得
+//     const date = json.forecasts[0].date;
+
+//     //天気画像取得
+//     const weatherImage = json.forecasts[0].image.url;
+
+//     //最高気温取得
+//     const maxTemp = json.forecasts[0].temperature.max.celsius;
+
+//     //最低気温取得
+//     const minTemp = json.forecasts[0].temperature.min.celsius;
+
+//     console.log(json);
+//     return {
+//       cityName: cityName,
+//       date: date,
+//       weatherImage: weatherImage,
+//       maxTemp: maxTemp,
+//       minTemp: minTemp,
+//     };
+//   } catch (error) {
+//     console.error(error.message);
+//     return null;
+//   }
+// }
 
 //取得したデータをhtml要素に入れる
 function setDataToHtml(weatherData) {
